1d380e4adb2eae6a6c84ccd1d355cf008d35d1cd
Andrew Tipton <andrew@kiwidrew.com>
2012-09-25 14:57:59 +0800

    replace Makefile with something simpler (but which works and has no Arduino dependency) and update the Configuration.h file with the correct board def.

--- src/Configuration.h
+++ src/Configuration.h
@@ -17,7 +17,7 @@
 // Gen 3 Plus = 21
 // gen 3  Monolithic Electronics = 22
 // Gen3 PLUS for TechZone Gen3 Remix Motherboard = 23
-#define MOTHERBOARD 33
+#define MOTHERBOARD 9
 
 //// Thermistor settings:
 // 1 is 100k thermistor
